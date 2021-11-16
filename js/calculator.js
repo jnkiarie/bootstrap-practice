a3bda097e503c55c4d2c39c118965c404f2fd526
jnkiarie <92355202+jnkiarie@users.noreply.github.com>
2021-11-16 21:52:27 +0300

    calc update

--- js/calculator.js
+++ js/calculator.js
@@ -1,46 +1,42 @@
-const additionFunction = function(number1, number2){
+const additionFunction = function(number1, number2) {
     return number1 + number2;
-}
-const divisionFunction = function(number1, number2){
+  }
+  const divisionFunction = function(number1, number2) {
     return number1 / number2;
-}
-
-const multiplyFunction = function(number1, number2){
+  }
+  
+  const multiplyFunction = function(number1, number2) {
     return number1 * number2;
-}
-
-const subtractFunction = function(number1, number2){
+  }
+  
+  const subtractFunction = function(number1, number2) {
     return number1 - number2;
-}
-
-var num1 = prompt("Enter first number");
-var num2 = prompt("Enter the second number");
-var calc = prompt("Enter the Operator")
-
-// convert our strings to numbers
-var num1Int = parseInt(num1);
-var num2Int = parseInt(num2);
-
-if (calc == "*") {
-    let result = multiplyFunction(num1Int, num2Int);
+  }
+  
+  var num1 = parseInt(prompt("Enter first number"));
+  var num2 = parseInt(prompt("Enter the second number"));
+  var calc = (prompt("Enter the Operator"));
+  
+  
+  
+  if (calc == "*") {
+    let result = multiplyFunction(num1, num2);
     alert(result);
-}
-else if (calc == "+"){
-    let result = additionFunction(num1Int, num2Int);
+  } else if (calc == "+") {
+    let result = additionFunction(num1, num2);
     alert(result);
-}
-else if (calc == "-"){
-    let result = subtractFunction(num1Int, num2Int);
+  } else if (calc == "-") {
+    let result = subtractFunction(num1, num2);
     alert(result);
-}
-else{
-    let result = divisionFunction(num1Int, num2Int);
-    alert(result);
-}
-
-
-
-// let result = additionFunction(num1Int, num2Int);
-// in pure JS we log results via the console log
-// use the alert when using html 
-// alert(result);
+  } else if (calc == "/") {
+      let result = divisionFunction(num1, num2);
+      alert(result);
+    }
+  
+  
+  
+    // let result = additionFunction(num1Int, num2Int);
+    // in pure JS we log results via the console log
+    // use the alert when using html 
+    // alert(result);
+  
